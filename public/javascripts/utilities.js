d7fbb3c3345fe3d340b23936465066bd76e6f6a3
James Adam <james@lazyatom.com>
2011-01-28 15:07:48 +0000

    This log statement is particularly noisy.

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -51,7 +51,7 @@ $.fn.isScrollAtBottom = function() {
   var panel = this;
   pos = panel[0].scrollTop - (panel[0].scrollHeight - panel[0].clientHeight);
   if (panel.attr("id") == "chat") {
-    console.log(panel.attr("id") + ": pos=" + pos + " pos+last=" + (pos + $(panel).children().last().outerHeight()));
+    //console.log(panel.attr("id") + ": pos=" + pos + " pos+last=" + (pos + $(panel).children().last().outerHeight()));
   }
   return pos == 0 || (pos + $(panel).children().last().outerHeight()) == 0;
 }
